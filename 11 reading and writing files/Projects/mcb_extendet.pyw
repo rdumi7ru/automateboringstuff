877abdee1a7d28c09c96dc21ccc576ea20115260
Razvan Dumitru <razvan@oc2460545814.ibm.com>
2018-10-26 15:52:10 +0300

    Added mcb.pyw and mcb_extendet files

---
+++ 11 reading and writing files/Projects/mcb_extendet.pyw
@@ -0,0 +1,34 @@
+#!/usr/bin/python3.4
+# mcb.pyw - Saves and loads pieces of text to the clipboard.
+# Usage: python3.4 mcb_extendet.pyw save <keyword> - Saves clipboard to keyword.
+#        python3.4 mcb_extendet.pyw <keyword> - Loads keyword to clipboard.
+#        python3.4 mcb_extendet.pyw list - Loads all keywords to clipboard.
+#        python3.4 mcb_extendet.pyw del - Delete's the key that you enter
+
+import shelve, pyperclip, sys
+
+mcbShelf = shelve.open('mcb')
+
+# Save clipboard 
+if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
+    mcbShelf[sys.argv[2]] = pyperclip.paste()
+
+elif len(sys.argv) == 2:
+    # List keywords, load content, and delete key.
+    if sys.argv[1].lower() == 'list':
+        pyperclip.copy(str(list(mcbShelf.keys())))
+    elif sys.argv[1].lower() == 'del':
+        # Get the key that will be deleted.
+        key = input(('Enter the keyword you want do delete:\n'))
+        del mcbShelf[key]
+    elif sys.argv[1].lower() == 'delete':
+        mcbShelf.clear()
+    elif sys.argv[1] in mcbShelf:
+        pyperclip.copy(mcbShelf[sys.argv[1]])
+mcbShelf.close()
+
+
+# I also could use:
+#elif sys.argv[1].lower() == 'delete':
+#        for k in list(mcbShelf.keys()):
+#            del mcbShelf[k]
--- test.py
+++ test.py
@@ -1,31 +1,13 @@
-#!/usr/bin/python
+import shelve
 
-import grp, pwd, os
-from ctypes import *
-from ctypes.util import find_library
+key = input(('Enter the keyword:\n'))
 
-libc = cdll.LoadLibrary(find_library('libc'))
+shelfFile = shelve.open('/home/razvan/python/filename')
+shelfFile['cats'] = ['Pooka', 'Simon']
+shelfFile['dogs'] = ['Ursu', 'Tookie']
 
-getgrouplist = libc.getgrouplist
-# 50 groups should be enought?
-ngroups = 50
-getgrouplist.argtypes = [c_char_p, c_uint, POINTER(c_uint * ngroups), POINTER(c_int)]
-getgrouplist.restype = c_int32
+print(list(shelfFile.keys()))
+del shelfFile[key]
 
-grouplist = (c_uint * ngroups)()
-ngrouplist = c_int(ngroups)
-
-user = pwd.getpwuid(2540485)
-
-ct = getgrouplist(user.pw_name, user.pw_gid, byref(grouplist), byref(ngrouplist))
-
-# if 50 groups was not enought this will be -1, try again
-# luckily the last call put the correct number of groups in ngrouplist
-if ct < 0:
-    getgrouplist.argtypes = [c_char_p, c_uint, POINTER(c_uint *int(ngrouplist.value)), POINTER(c_int)]
-    grouplist = (c_uint * int(ngrouplist.value))()
-    ct = getgrouplist(user.pw_name, user.pw_gid, byref(grouplist), byref(ngrouplist))
-
-for i in xrange(0, ct):
-    gid = grouplist[i]
-    print grp.getgrgid(gid).gr_name
+print(list(shelfFile.keys()))
+shelfFile.close()
