8d88ed79c20dd5ded8b6dc973cf0a6480b794a86
Razvan Dumitru <razvan@oc2460545814.ibm.com>
2018-10-29 19:20:59 +0200

    Update

--- 11 reading and writing files/Projects/mad_libs.py
+++ 11 reading and writing files/Projects/mad_libs.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3.4
 
-import re
+"""
+    Reads text files and lets the user add their own text anywhere
+    the word ADJECTIVE, NOUN, ADVERB, or VERB appears in the text file.
+"""
+filler = ['ADJECTIVE', 'NOUN', 'ADVERB', 'VERB']
+answer = []
+#text = 'The ADJECTIVE panda walked to the NOUN and then VERB. A nearby NOUN was unaffected by these events.'
 
-madText = open('/home/razvan/python/madText.txt', 'w')
-text = 'The ADJECTIVE panda walked to the NOUN and then VERB. A nearby NOUN was unaffected by these events.'
+file = open('/home/razvan/python/sentence.txt', 'r')
+content = file.read().split()
 
-madText.write(text)
-madText.close()
+for word in content:
+    if word.strip('.|!|?|,') in filler:
+        answer.append(input('Enter a {0}: '.format(word)))
+    else:
+        answer.append(word)
 
-content = re.split('\W+', text)
-# output = []
-
-for i in content:
-    if i == 'ADJECTIVE':
-        replace_regex = re.compile(r'(ADJECTIVE)')
-        adjective = input(('Please enter an Adjective\n'))
-        output = replace_regex.sub(adjective, str(content))
-    
-    elif i == 'NOUN':
-        replace_regex = re.compile(r'NOUN')
-        noun = input(('Please enter a Noun\n'))
-        output = replace_regex(noun, str(content))
-
-    elif i == 'VERB':
-        replace_regex = re.compile(r'(')
-
+file = open('/home/razvan/python/sentence.txt', 'w')
+file.write(' '.join(answer))
+file.close()
