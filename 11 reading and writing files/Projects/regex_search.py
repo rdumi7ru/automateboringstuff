61d7abc28ce9481371b335e73de49f6534ffb715
Razvan Dumitru <razvan@oc2460545814.ibm.com>
2018-10-31 09:20:40 +0200

    modified regex_search

--- 11 reading and writing files/Projects/regex_search.py
+++ 11 reading and writing files/Projects/regex_search.py
@@ -3,8 +3,28 @@
 import os
 import re
 
-directory = input('Enter a directory name:\n')
-
+os.chdir('/home/razvan/python')
 regex = input(('Enter the patern:\n'))
 regexSearch = re.compile(regex, re.I)
-result = regexSearch.findall()
+directory = input('Enter a directory name:\n')
+dir = os.path.abspath(directory)
+if os.path.exists(dir) == True:
+    print(dir + ' folder exists')
+    allFiles = os.listdir(dir)
+    results = []
+    for file in allFiles:
+        if file.endswith('.txt'):
+            content = open(os.path.join(dir, file))
+            holder = []
+            for line in content.readlines():
+                if regexSearch.search(line):
+                    holder.append( 'MATCH: ' + line + '\n')
+            holder = [file + '\n' + '*'*20 + '\n' + ''.join(holder)]
+            holder += '\n'*2
+            results.append(holder)
+            content.close()
+    for result in results:
+        print(result)
+else:
+    print('Folder ' + os.path.basename(dir) + ' does not exist!')
+# result = regexSearch.findall()
--- 11 reading and writing files/Projects/regex_search_test.py
+++ 11 reading and writing files/Projects/regex_search_test.py
@@ -1,2 +1,52 @@
 #!/usr/bin/python3
 
+#directory searcher: searches through a directory for text files and looks for what files contain a given regular expression
+#Usage: python dirsearch.py <regexpression> <directory>
+ 
+import sys, os, re
+ 
+os.chdir('/home/razvan/python')
+ 
+#TODO: GET COMMAND LINE ARGUMENTS 
+if len(  sys.argv  ) >= 2:
+          directory = "." #if the user gives no directory path
+          if len( sys.argv) == 3:
+                    directory = sys.argv[2] #if there exists a given directory
+#TODO: ISOLATE DIRECTORY AND REGULAR EXPRESSION 
+          directory = os.path.abspath(directory)
+          regularExpression = sys.argv[1]
+          regexpression = re.compile(regularExpression)
+#TODO: CHECK IF DIRECTORY EXISTS
+          if not os.path.exists(directory):
+                    print("directory does not exist")
+          if not regexpression.pattern:
+                    print("Regular expression is invalid")
+          else:
+                    #TODO: GET LIST OF FILES IN DIRECTORY
+                    allfilePaths = os.listdir(directory)
+                    results = []
+                    #TODO: LOOP AND CHECK THROUGH THE TEXT FILES 
+                    for filePath in allfilePaths:
+                              ext = os.path.basename(filePath)
+                              if not "txt" in ext:
+                                        continue
+                              #TODO: READ THE CONTENT AND MATCH ONLY IF TEXTFILE
+                              file = open(filePath)
+                              holder = []
+                              #RESULT HOLDER 
+                              for line in file.readlines():
+                                        #TODO: IF MATCHED CREATE STRING AND STORE INTO UPDATE ARRAY 
+                                        if regexpression.search(line):
+                                                  holder.append(  "MATCH: " + line + "\n" )
+                              #TODO: CLOSE FILE WHEN DONE READING
+                              if len( holder ) <= 0:
+                                        continue
+                              holder = filePath + "\n" + "*"*20 + "\n" + "".join(holder)
+                              holder +="\n"*2
+                              results.append(holder)
+                              file.close()
+                    #TODO: PRINT RESULT
+                    for result in results:
+                              print(result)
+else:
+          print("Usage: python dirsearch.py <regexpression> <directory>(optional)")
