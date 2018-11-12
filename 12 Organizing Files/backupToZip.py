c06bbc5dbe110ed97eb4b9e609a93ed3a67f7233
DMR <dumitru.rm@gmail.com>
2018-11-12 18:18:33 +0200

    Finished backupToZip.py

--- 12 Organizing Files/backupToZip.py
+++ 12 Organizing Files/backupToZip.py
@@ -6,30 +6,35 @@ import zipfile
 import os
 
 def backupToZip(folder):
-    os.chdir('/home/razvan/python/zip_project')
-    # Backup the entire contents of "folder" into a ZIP file.
-    folder = os.path.abspath(folder)    # make sure folder is absolute
+    # Backup the entire contents of "folder" into a zip file.
 
-    # Figure out the filename this code should use based on
-    # what files allready exists.
+    folder = os.path.abspath(folder) # Make sure folder is absolute
+    os.chdir(folder)
+    # Figure out the filename this code should used based on what
+    # files already exist.
     number = 1
     while True:
         zipFilename = os.path.basename(folder) + '_' + str(number) + '.zip'
         if not os.path.exists(zipFilename):
-                break
-        number += 1
-        
-    # Create the ZIP file.
+            break
+        number = number + 1
+
+    # Create the zip file.
     print('Creating %s...' % (zipFilename))
     backupZip = zipfile.ZipFile(zipFilename, 'w')
 
     # Walk the entire folder tree and compress the files in each folder.
-    for folderName, subFolders, filenames in os.walk(folder):
-        print('Adding files in %s...' % (folderName))
-        # Add the current folder to the ZIP file.
-        backupZip.write(folderName)
-        
-        # Add all the filles in this folder to the ZIP file.
+    for foldername, subfolders, filenames in os.walk(folder):
+        print('Adding files in %s...' % (foldername))
+        # Add the current folder to the zip file.
+        backupZip.write(foldername)
+
+        # Add all the files in this folder to the zip file.
         for filename in filenames:
-            
-        
+            if filename.startswith(os.path.basename(folder) + '_') and filename.endswith('.zip'):
+                continue # Don't backup the backup zip files.
+            backupZip.write(os.path.join(foldername, filename))
+    backupZip.close()
+    print('Done.')
+
+backupToZip('/home/razvan/python/backupToZip')
