87cbcff4b44247344d0f0ad48f9d929b084f64e7
DMR <dumitru.rm@gmail.com>
2018-11-13 19:09:30 +0200

    Created selectiveCopy.py

---
+++ 12 Organizing Files/Projects/selectiveCopy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import os
+import shutil
+
+def selectiveCopy(folder):
+    # Move all .txt and .jpg to selectiveCopy2 folder
+    folder = os.path.abspath(folder)
+
+    # Walk the directory and search for files that ends with .txt and .jpg
+    for folderName, subFolders, filenames in os.walk(folder):
+        print('Searching for .txt and .jpg files in %s...' % (folderName))
+        for filename in filenames:
+            if filename.endswith('.jpg') and filenames.endswith('.txt'):
+                shutil.copy(os.path.join(folder, filename),)
