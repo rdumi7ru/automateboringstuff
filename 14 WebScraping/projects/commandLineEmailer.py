39e8ba061a2eeb2fa50907d05f699b452f3f2718
dmr <dumitru.rm@gmail.com>
2019-02-10 23:59:33 +0200

    Started commandLineEmailer.py

---
+++ 14 WebScraping/projects/commandLineEmailer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# commandLineEmailer.py takes an email address and 
+# a sting of text from the command line and then
+# using selenium to log into the gmail and send 
+# an email.
+
+import sys
+from selenium import webdriver
